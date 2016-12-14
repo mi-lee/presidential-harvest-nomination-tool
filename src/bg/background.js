67b3d1c11547bd650d52899cffc79a69e8937c1d
Sonal Ranjit <sonal.ranjit3@gmail.com>
2016-12-14 14:50:40 -0500

    added new form url and field entry values

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,5 +1,10 @@
-var GOOGLE_FORMS_URL = '';
-var FORM_FIELDS = 'formResponse?ifq&entry.1955598399=';
+var GOOGLE_FORMS_URL = 'https://docs.google.com/forms/d/1kuwxu2lXYSRpkwBj4o9kwjURZL3hgk-mSFoK4qkC4ZI/formResponse?ifq';
+//var FORM_FIELDS = 'formResponse?ifq&entry.1955598399=';
+var NAME_FIELD = '&entry.604638068=';
+var EVENTNAME_FIELD = '&entry.1800984457=';
+var URL_FIELD = '&entry.242612017=';
+// Form Name entry value = entry.604638068
+// Event URL Entry Value = entry.242612017
 
 chrome.browserAction.onClicked.addListener( function( tab ) {
   console.log( "tab = ", tab );
@@ -9,7 +14,7 @@ chrome.browserAction.onClicked.addListener( function( tab ) {
 
   chrome.identity.getProfileUserInfo( function( userInfo ) {
     return $.get( {
-      url: GOOGLE_FORMS_URL + FORM_FIELDS + userInfo.email + '&submit=Submit',
+      url: GOOGLE_FORMS_URL + NAME_FIELD +'NAME'+ EVENTNAME_FIELD + 'Guerilla Archiving'+URL_FIELD+'www.google.ca'+ '&submit=Submit',
       success: function( res ) {
         window.open( notificationToolUrl + currentURL );
       },
