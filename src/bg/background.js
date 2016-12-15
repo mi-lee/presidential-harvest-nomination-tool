149837469dffa78bc7da3a03d201f53923a6f9c1
Sonal Ranjit <sonal.ranjit3@gmail.com>
2016-12-14 20:48:08 -0500

    cleaning up a little bit

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,11 +1,7 @@
 var GOOGLE_FORMS_URL = 'https://docs.google.com/forms/d/1kuwxu2lXYSRpkwBj4o9kwjURZL3hgk-mSFoK4qkC4ZI/formResponse?ifq';
-//var FORM_FIELDS = 'formResponse?ifq&entry.1955598399=';
 var NAME_FIELD = '&entry.604638068=';
 var EVENTNAME_FIELD = '&entry.1800984457=';
 var URL_FIELD = '&entry.242612017=';
-// Form Name entry value = entry.604638068
-// Event URL Entry Value = entry.242612017
-//curl 'https://docs.google.com/forms/d/1kuwxu2lXYSRpkwBj4o9kwjURZL3hgk-mSFoK4qkC4ZI/formResponse?ifq'&entry.604638068=NAME&entry.1800984457=something'&entry.242612017=url&submit=Submit'
 
 chrome.browserAction.onClicked.addListener( function( tab ) {
   console.log( "tab = ", tab );
